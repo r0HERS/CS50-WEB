63b2fb21ec03f11fc840e49eb3bb0a3cb09194e3
Renan Rohers Salvador <111884210+r0HERS@users.noreply.github.com>
2024-07-27 14:55:18 -0300

    Delete capstone directory

--- capstone/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Project,Invite,Task,Comments
-
-admin.site.register(User)
-admin.site.register(Project)
-admin.site.register(Invite)
-admin.site.register(Task)
-admin.site.register(Comments)
